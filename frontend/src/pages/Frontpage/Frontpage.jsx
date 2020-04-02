a2623978014b36e2a91f7b0b019d921ef604315c
Ryan Lim <ryanlimenwei@gmail.com>
2020-04-02 16:12:02 +1300

    Add Join Icon to card in Frontpage
    Added a Material UI icon to the card.

--- frontend/src/pages/Frontpage/Frontpage.jsx
+++ frontend/src/pages/Frontpage/Frontpage.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import AddIcon from "@material-ui/icons/Add";
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
 import { Container } from "@material-ui/core";
 
 import styles from "./frontpage.module.css";
@@ -18,8 +19,8 @@ const Frontpage = ({ handleClick }) => {
         />
         <CardButton
           handleClick={handleClick}
-          label="Create a party"
-          icon={<AddIcon classes={{ root: styles.icon }} />}
+          label="Join a party"
+          icon={<PeopleAltIcon classes={{ root: styles.icon }} />}
         />
       </div>
     </Container>
