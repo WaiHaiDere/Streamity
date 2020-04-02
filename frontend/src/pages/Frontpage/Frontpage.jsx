aed524cabd18c3092e91fac1245b781a2c5b079d
Ryan Lim <ryanlimenwei@gmail.com>
2020-04-02 15:13:19 +1300

    Add Frontpage components
    Basic start up for Front page.

--- frontend/src/pages/Frontpage/Frontpage.jsx
+++ frontend/src/pages/Frontpage/Frontpage.jsx
@@ -1,26 +1,44 @@
 import React from "react";
 import PropTypes from "prop-types";
-import logo from "../../logo.svg";
 import "../../App.css";
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import CardActionArea from '@material-ui/core/CardActionArea';
+
+import styles from './frontpage.module.css';
 
 const Frontpage = ({ handleChange }) => {
-  console.log(handleChange);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Streamity
+      <div>
+      <Card classes={{root: styles.card}}>
+        <CardActionArea>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Word of the Day
+            </Typography>
+            <Typography variant="h5" component="h2">
+              Yeet
+            </Typography>
+            <Typography color="textSecondary">
+              adjective
+            </Typography>
+            <Typography variant="body2" component="p">
+              Meaning throw
+              <br />
+              {'"Yeet that bitch"'}
+            </Typography>
+          </CardContent>
+        <CardActions>
+          <Button size="small">Learn More</Button>
+        </CardActions>
+        </CardActionArea>
+      </Card>
+      </div>
     </div>
   );
 };
