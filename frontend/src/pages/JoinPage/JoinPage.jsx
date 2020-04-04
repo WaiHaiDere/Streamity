1e6739bd162f5dec960b7a9eab7148f3b1996b7e
WaiHaiDereMAC <tyger.kong@hotmail.com>
2020-04-04 14:25:31 +1300

    Set up for joining page

---
+++ frontend/src/pages/JoinPage/JoinPage.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import PropTypes from "prop-types";
+import AddIcon from "@material-ui/icons/Add";
+import PeopleAltIcon from '@material-ui/icons/PeopleAlt';
+import { Container, Typography } from "@material-ui/core";
+
+import styles from "./frontpage.module.css";
+import CardButton from "../../components/CardButton";
+
+const JoinPage = ({ handleClick }) => {
+  return (
+    <div className={styles.backgroundContainer}>
+      <Container maxWidth="sm" classes={{ root: styles.pageContainer }}>
+      <div className={styles.textBox}>
+        <Typography variant="h1" classes={{ root: styles.title }}>
+          Streamity
+        </Typography>
+        <Typography variant="h4" classes={{ root: styles.slogan }}>
+          Create. Join. Stream.
+        </Typography>
+      </div>
+      <div className={styles.buttonBox}>
+        <CardButton
+          handleClick={handleClick}
+          label="Create a party"
+          icon={<AddIcon classes={{ root: styles.icon }} />}
+        />
+        <CardButton
+          handleClick={handleClick}
+          label="Join a party"
+          icon={<PeopleAltIcon classes={{ root: styles.icon }} />}
+        />
+        </div>
+      </Container>
+    </div>
+  );
+};
+
+JoinPage.defaultProps = {
+  handleClick: () => {},
+};
+
+JoinPage.propTypes = {
+  handleClick: PropTypes.func,
+};
+
+export default JoinPage;
---
+++ frontend/src/pages/JoinPage/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import JoinPage from "../../containers/JoinPageContainer";
+
+import JoinPage from "./JoinPage";
+
+export default () => {
+  return (
+    <JoinPage>
+      <JoinPage />
+    </JoinPage>
+  );
+};
