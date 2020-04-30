3a9b7efb5339cdfa686b37d99fb4f08cb63966ee
Ryan Lim <ryanlimenwei@gmail.com>
2020-04-30 20:43:29 +1200

    Add authCode for spotify login
    * Saves spotify login auth code in global context.

--- frontend/src/containers/BufferpageContainer.jsx
+++ frontend/src/containers/BufferpageContainer.jsx
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from "react";
+import { useGlobalState } from "../hooks/GlobalState/GlobalStateProvider";
+import keys from "../hooks/GlobalState/keys";
+
 
 const BufferpageContainer = ({ children }) => {
 
   const [isLoading, setIsLoading] = useState(true);
   const [authCode, setAuthCode] = useState("");
 
+  const {
+    getGlobalState,
+    existsInGlobalState,
+    putGlobalState,
+  } = useGlobalState();
+
   useEffect(() => {
-    getParameterByName("code");
-    console.log(authCode);
-  });
+    const spotifyCode = getParameterByName("code");
+    putGlobalState({key: keys.SPOTIFY_AUTH_CODE, value: {spotifyAuthCode: spotifyCode}});
+    
+  },[]);
 
   const setLoading = () => {
     setIsLoading(isLoading => !isLoading)
@@ -19,10 +29,10 @@ const BufferpageContainer = ({ children }) => {
   const getParameterByName = (name, url) => {
     if (!url) url = window.location.href; // If there is no input param, it finds it from the URL.
     name = name.replace(/[\[\]]/g, '\\$&');
-    var regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
+    const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)'),
         results = regex.exec(url);
-    var authCode = decodeURIComponent(results[2].replace(/\+/g, ' '));
-    setAuthCode(authCode);
+    const authCode = decodeURIComponent(results[2].replace(/\+/g, ' '));
+    return authCode;
   };
 
   const newProps = { isLoading, setLoading };
--- frontend/src/hooks/GlobalState/keys.js
+++ frontend/src/hooks/GlobalState/keys.js
@@ -1,5 +1,6 @@
 const keys = {
   SESSION: "SESSION",
+  SPOTIFY_AUTH_CODE: "SPOTIFY_AUTH_CODE",
 };
 
 export default keys;
