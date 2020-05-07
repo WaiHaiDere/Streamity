53fc894f855f98fc3b6c60e0acb9c7bdf136bb58
Michelle Tan <michellemtan@outlook.com>
2020-05-08 11:08:47 +1200

    Add avatar icon to header

--- frontend/src/components/RightDrawer/RightDrawer.jsx
+++ frontend/src/components/RightDrawer/RightDrawer.jsx
@@ -1,63 +1,45 @@
-import React from 'react';
+import React from "react";
 import {
-    Drawer,
-    List,
-    Divider,
-    ListItem,
-    ListItemIcon,
-    ListItemText,
-    Typography,
-    TextField,
-    IconButton
-} from '@material-ui/core';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import SendIcon from '@material-ui/icons/Send';
+  Drawer,
+  List,
+  Divider,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+  TextField,
+  IconButton,
+} from "@material-ui/core";
+import InboxIcon from "@material-ui/icons/MoveToInbox";
+import MailIcon from "@material-ui/icons/Mail";
+import SendIcon from "@material-ui/icons/Send";
 
-import PropTypes from 'prop-types';
-import styles from '../RightDrawer/rightdrawer.module.css';
+import PropTypes from "prop-types";
+import styles from "../RightDrawer/rightdrawer.module.css";
 
-const RightDrawer = ({handlclick, pin}) => {
+import { Avatar } from "@material-ui/core";
+import MusicNoteIcon from "@material-ui/icons/MusicNote";
+
+const RightDrawer = ({ handlclick, pin }) => {
   return (
     <div>
       <Drawer
         variant="permanent"
         classes={{
-          paper: styles.Drawer
+          paper: styles.Drawer,
         }}
         anchor="right"
       >
         <div className={styles.DrawerHeader}>
-           <Typography variant='h4'>
-               PIN #{pin}
-           </Typography>
+          <Typography variant="h5">PIN #{pin}</Typography>
+          <Avatar className={styles.avatar}>M</Avatar>
         </div>
-        <div/>
-        <Divider />
-        <List>
-          {['Inbox', 'Starred', 'Send email', 'Drafts'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
-        <Divider />
-        <List>
-          {['All mail', 'Trash', 'Spam'].map((text, index) => (
-            <ListItem button key={text}>
-              <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItem>
-          ))}
-        </List>
+        <div />
         <div className={styles.ChatBox}>
-          <TextField 
-          label="Type your message here" 
-            />
-            <IconButton>
-            <SendIcon/>
-            </IconButton>
+          <TextField label="Type your message here" />
+          <IconButton>
+            <SendIcon />
+          </IconButton>
         </div>
       </Drawer>
     </div>
@@ -65,11 +47,11 @@ const RightDrawer = ({handlclick, pin}) => {
 };
 
 RightDrawer.defaultProps = {
-    handleClick: () => {}
+  handleClick: () => {},
 };
 
 RightDrawer.propTypes = {
-    handleClick: PropTypes.func,
+  handleClick: PropTypes.func,
 };
 
 export default RightDrawer;
