bab3f6f4a47be0fb0d98f74e2191f206837a6afc
Michelle Tan <michellemtan@outlook.com>
2020-05-07 19:20:21 +1200

    Add background and title to centre panel

--- frontend/src/pages/MediaViewPage/MediaViewPage.jsx
+++ frontend/src/pages/MediaViewPage/MediaViewPage.jsx
@@ -1,19 +1,25 @@
 import React from "react";
 import PropTypes from "prop-types";
-// import { Container, Typography } from "@material-ui/core";
-
-// import styles from "./mediaviewpage.module.css";
+import { Container, Typography } from "@material-ui/core";
+import styles from "./mediaviewpage.module.css";
 import LeftDrawer from "../../components/LeftDrawer/LeftDrawer";
 import RightDrawer from "../../components/RightDrawer/RightDrawer";
 
 const MediaViewPage = ({ handleClick, listOfSearchResults, pin }) => {
   return (
+    <div className={styles.backgroundContainer}>
+      <div className={styles.centrePanel}>
+          <Typography variant="h1" classes={{ root: styles.title }}>
+            Streamity
+          </Typography>
+    </div>
     <div>
       <LeftDrawer
         handleClick={handleClick}
         listOfSearchResults={listOfSearchResults}
       />
       <RightDrawer handleClick={handleClick} pin={pin} />
+    </div>
     </div>
   );
 };
