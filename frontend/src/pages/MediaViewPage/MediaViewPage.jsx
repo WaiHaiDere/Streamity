be7adf58ce6ea7836b007fa084130b798c8daf11
WaiHaiDereMAC <tyger.kong@hotmail.com>
2020-05-09 11:48:48 +1200

    checks for token on load

--- frontend/src/pages/MediaViewPage/MediaViewPage.jsx
+++ frontend/src/pages/MediaViewPage/MediaViewPage.jsx
@@ -15,60 +15,68 @@ const MediaViewPage = ({
   memberList,
   token,
 }) => {
-  const handleScriptError = () => {
-    console.log("ERROR LOADING SCRIPT");
-  };
+  let handleScriptError;
+  let handleScriptLoad;
 
-  const handleScriptLoad = () => {
-    window.onSpotifyWebPlaybackSDKReady = () => {
-      const player = new window.Spotify.Player({
-        name: "Streamity",
-        getOAuthToken: (cb) => {
-          cb(token);
-        },
-      });
-
-      // Error handling
-      player.addListener("initialization_error", ({ message }) => {
-        console.error(message);
-      });
-      player.addListener("authentication_error", ({ message }) => {
-        console.error(message);
-      });
-      player.addListener("account_error", ({ message }) => {
-        console.error(message);
-      });
-      player.addListener("playback_error", ({ message }) => {
-        console.error(message);
-      });
-
-      // Playback status updates
-      player.addListener("player_state_changed", (state) => {
-        console.log(state);
-      });
-
-      // Ready
-      player.addListener("ready", ({ device_id }) => {
-        console.log("Ready with Device ID", device_id);
-      });
-
-      // Not Ready
-      player.addListener("not_ready", ({ device_id }) => {
-        console.log("Device ID has gone offline", device_id);
-      });
-
-      // Connect to the player!
-      player.connect();
+  if (token) {
+    handleScriptError = () => {
+      console.log("ERROR LOADING SCRIPT");
     };
-  };
+
+    handleScriptLoad = () => {
+      window.onSpotifyWebPlaybackSDKReady = () => {
+        const player = new window.Spotify.Player({
+          name: "Streamity",
+          getOAuthToken: (cb) => {
+            cb(token);
+          },
+        });
+
+        // Error handling
+        player.addListener("initialization_error", ({ message }) => {
+          console.error(message);
+        });
+        player.addListener("authentication_error", ({ message }) => {
+          console.error(message);
+        });
+        player.addListener("account_error", ({ message }) => {
+          console.error(message);
+        });
+        player.addListener("playback_error", ({ message }) => {
+          console.error(message);
+        });
+
+        // Playback status updates
+        player.addListener("player_state_changed", (state) => {
+          console.log(state);
+        });
+
+        // Ready
+        player.addListener("ready", ({ device_id }) => {
+          console.log("Ready with Device ID", device_id);
+        });
+
+        // Not Ready
+        player.addListener("not_ready", ({ device_id }) => {
+          console.log("Device ID has gone offline", device_id);
+        });
+
+        // Connect to the player!
+        player.connect();
+      };
+    };
+  }
   console.log(memberList);
   return (
     <div>
-      <Script
-        url="https://sdk.scdn.co/spotify-player.js"
-        onLoad={handleScriptLoad()}
-        onError={handleScriptError()}
-      />
+      {token ? (
+        <Script
+          url="https://sdk.scdn.co/spotify-player.js"
+          onLoad={handleScriptLoad()}
+          onError={handleScriptError()}
+        />
+      ) : null}
+
       <LeftDrawer
         handleClick={handleClick}
         listOfSearchResults={listOfSearchResults}
