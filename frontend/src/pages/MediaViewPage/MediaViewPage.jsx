2a8647ca2e919ab1fc30da8f7cab1d2949ccbd1d
WaiHaiDereMAC <tyger.kong@hotmail.com>
2020-05-09 11:49:34 +1200

    token check

--- frontend/src/pages/MediaViewPage/MediaViewPage.jsx
+++ frontend/src/pages/MediaViewPage/MediaViewPage.jsx
@@ -15,57 +15,53 @@ const MediaViewPage = ({
   memberList,
   token,
 }) => {
-  let handleScriptError;
-  let handleScriptLoad;
+  const handleScriptError = () => {
+    console.log("ERROR LOADING SCRIPT");
+  };
 
-  if (token) {
-    handleScriptError = () => {
-      console.log("ERROR LOADING SCRIPT");
+  const handleScriptLoad = () => {
+    window.onSpotifyWebPlaybackSDKReady = () => {
+      const player = new window.Spotify.Player({
+        name: "Streamity",
+        getOAuthToken: (cb) => {
+          cb(token);
+        },
+      });
+
+      // Error handling
+      player.addListener("initialization_error", ({ message }) => {
+        console.error(message);
+      });
+      player.addListener("authentication_error", ({ message }) => {
+        console.error(message);
+      });
+      player.addListener("account_error", ({ message }) => {
+        console.error(message);
+      });
+      player.addListener("playback_error", ({ message }) => {
+        console.error(message);
+      });
+
+      // Playback status updates
+      player.addListener("player_state_changed", (state) => {
+        console.log(state);
+      });
+
+      // Ready
+      player.addListener("ready", ({ device_id }) => {
+        console.log("Ready with Device ID", device_id);
+      });
+
+      // Not Ready
+      player.addListener("not_ready", ({ device_id }) => {
+        console.log("Device ID has gone offline", device_id);
+      });
+
+      // Connect to the player!
+      player.connect();
     };
+  };
 
-    handleScriptLoad = () => {
-      window.onSpotifyWebPlaybackSDKReady = () => {
-        const player = new window.Spotify.Player({
-          name: "Streamity",
-          getOAuthToken: (cb) => {
-            cb(token);
-          },
-        });
-
-        // Error handling
-        player.addListener("initialization_error", ({ message }) => {
-          console.error(message);
-        });
-        player.addListener("authentication_error", ({ message }) => {
-          console.error(message);
-        });
-        player.addListener("account_error", ({ message }) => {
-          console.error(message);
-        });
-        player.addListener("playback_error", ({ message }) => {
-          console.error(message);
-        });
-
-        // Playback status updates
-        player.addListener("player_state_changed", (state) => {
-          console.log(state);
-        });
-
-        // Ready
-        player.addListener("ready", ({ device_id }) => {
-          console.log("Ready with Device ID", device_id);
-        });
-
-        // Not Ready
-        player.addListener("not_ready", ({ device_id }) => {
-          console.log("Device ID has gone offline", device_id);
-        });
-
-        // Connect to the player!
-        player.connect();
-      };
-    };
-  }
   console.log(memberList);
   return (
     <div>
