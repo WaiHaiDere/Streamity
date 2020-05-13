386b0e50897ad388d1b940c7bcb40f0a5f189cae
Michelle Tan <michellemtan@outlook.com>
2020-05-14 00:03:31 +1200

    Create error page display

---
+++ frontend/src/pages/ErrorPage/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ErrorPageContainer from "../../containers/ErrorpageContainer";
+import ErrorPage from "./ErrorPage";
+
+export default () => {
+  return (
+    <ErrorPageContainer>
+      <ErrorPage />
+    </ErrorPageContainer>
+  );
+};
