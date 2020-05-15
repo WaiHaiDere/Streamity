e2f5200fff11d223e47e18ac32880c4b0becdf74
Michelle Tan <michellemtan@outlook.com>
2020-05-15 16:08:24 +1200

    Add socket server to backend

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,23 @@ const cors = require("cors");
 app.use(express.json());
 app.use(cors());
 
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html');
+// });
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+});
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
 const pinRouter = require("./src/api-routes/room");
 const spotifyRouter = require("./src/api-routes/spotify");
 
