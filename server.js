ce7807c5b648598b8640ddac73dee6a9af37a35a
Joshua <joshua.sheng.fu@gmail.com>
2020-05-20 17:56:22 +1200

    Relocated root location

--- server.js
+++ server.js
@@ -16,8 +16,9 @@ const spotifyRouter = require("./src/api-routes/spotify");
 app.use("/api/spotify", spotifyRouter);
 app.use("/api/room", pinRouter);
 
+const port = process.env.PORT || 5002;
 db.connect().then(() => {
-  app.listen(5002, () => {
+  app.listen(port, () => {
     console.log("Listening on port 5002");
   });
 });
